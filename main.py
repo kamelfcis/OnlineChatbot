4330b27879c95e0cbc4e0d8a14b6a0c54bdea070
kamelfcis <kamelfcis@gmail.com>
2024-10-16 17:10:09 +0300

    Clear cache

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import load_model
-
-nltk.download('punkt')
-nltk.download('wordnet')
+ 
 lemmatizer = WordNetLemmatizer()
 
 app = Flask(__name__)
